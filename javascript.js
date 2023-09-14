bd73666ad55e76e60e3403ba1929a956999c11ba
Amanda <125668669+amcconnell1316@users.noreply.github.com>
2023-09-14 14:18:55 -0500

    Add hover effect and new sketch pad button

--- javascript.js
+++ javascript.js
@@ -1,19 +1,46 @@
-let numRows = 16;
 const sketchpadContainer = document.getElementById("sketchpadContainer");
+const newSketchpadButton = document.getElementById("newSketchpadBtn");
+newSketchpadButton.addEventListener('click', createNewSketchpad);
+createSketchpad(16);
 
-for(let i=1;i<=numRows;i++){
-    let currentRowDiv = document.createElement("div");
-    currentRowDiv.id = "row" + i;
-    currentRowDiv.classList.add("row");
+function createSketchpad(numRows){
+    for(let i=1;i<=numRows;i++){
+        let currentRowDiv = document.createElement("div");
+        currentRowDiv.id = "row" + i;
+        currentRowDiv.classList.add("row");
 
-    let currentColumnDiv;
-    for(let j=1;j<=numRows;j++){
-        let currentColumnDiv = document.createElement("div");
-        currentColumnDiv.id = "row" + i + "column" + j;
-        currentColumnDiv.classList.add("column");
-        currentRowDiv.appendChild(currentColumnDiv);
+        let currentColumnDiv;
+        for(let j=1;j<=numRows;j++){
+            let currentColumnDiv = document.createElement("div");
+            currentColumnDiv.id = "row" + i + "column" + j;
+            currentColumnDiv.classList.add("column");
+            currentColumnDiv.addEventListener('mouseenter', handleHover);
+            currentRowDiv.appendChild(currentColumnDiv);
+        }
+
+        sketchpadContainer.appendChild(currentRowDiv);
+
     }
+}
 
-    sketchpadContainer.appendChild(currentRowDiv);
+function handleHover(e){
+    const div = e.currentTarget;
+    div.classList.add("hovered");
+}
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createNewSketchpad(){
+    let numRows = Number(prompt("How many squares per side?"));
+    if(Number.isInteger(numRows) && numRows <= 100 && numRows > 0)
+    {
+        removeAllChildNodes(sketchpadContainer);
+        createSketchpad(numRows);
+    } else {
+        alert("Squares per side must be an integer less than 100");
+    }
 }
